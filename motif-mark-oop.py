636f8c78dadcbe06ff6bf86866b93644b3c57516
Jonathan Ogata <ogata1991@gmail.com>
2022-02-14 18:57:42 -0800

    awaiting motif finding function

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -11,16 +11,42 @@ import bioinfo
 #     return parser.parse_args()
 # args=get_args()
 
-fasta_dictionary=bioinfo.fasta_reader('test.fa')
-print(fasta_dictionary)
+fasta_dictionary=bioinfo.fasta_reader('test.fa') 
+length_of_contig=len(fasta_dictionary['>test_contig'])
 
-length_of_contig=12000
 
-exons = { # exon number: (start position, stop position)
-    1: (0,5000),
-    2: (7000,9000),
-    3: (11000, 12000)
+def exons_builder(file, contig):
+    '''generates a dictionary of exons as described below
+    input: a text file with exon seqeuences seperated by new lines
+    return: a dictionary of exons as described below'''
+    exons = {
+        # exon number: (start position, stop position)
+        }
+    with open(file) as e:
+        list_of_exons = e.read().splitlines()
+        exons=[]
+        for i in range(0,len(list_of_exons)):
+            start = contig.find(list_of_exons[i])
+            stop = len(list_of_exons[i]) + start
+            exons.append([start,stop])
+    exons.sort()
+    return(exons)
+
+class region:
+    '''desc here'''
+    __slots__ = ['contig', 'length', 'exons']
+    def __init__(self, contig, exon_file):
+        self.contig = contig
+        self.length=len(contig)
+        self.exons = exons_builder('exons.txt', contig)
+
+region_dictionary ={
+    # contig header: ( contig_length, exon_dictionary generated from exons builder )
 }
+
+for key in fasta_dictionary:
+    x = region(fasta_dictionary[key],'exons.txt')
+    region_dictionary[key]= (x.length, x.exons)
 
 
 ################################################
@@ -28,16 +54,22 @@ exons = { # exon number: (start position, stop position)
 ################################################
 
 
-def normalize(exons, surface_width):
+# set the surface dimensions
+surface_width = 100 #NEED TO SET THIS TO MAX CONTIG LENGTH
+scale = 20
+surface_height = len(region_dictionary)*scale 
+
+
+def normalize(exons, length_of_contig, surface_width):
     '''function that transforms exon genome coordinates into cairo coordinates
     input: exon dictionary, cairo surface width
     output: exon dictionary with transformed coordinates'''
 
     # transform exon dictionary
-    for key in exons:
-        exons[key] = (
-            round(surface_width * exons[key][0]/length_of_contig, 0),
-            round(surface_width * exons[key][1]/length_of_contig, 0)
+    for i in range(0,len(exons)):
+        exons[i] = (
+            round(surface_width * exons[i][0]/length_of_contig, 0),
+            round(surface_width * exons[i][1]/length_of_contig, 0)
             )
 
 def draw_intron(start, stop, level):
@@ -54,29 +86,49 @@ def draw_exon(start, stop, level):
     '''draws exons on cairo surface.
     input: exon start, exon stop, y coordinate of cairo surface (same as draw_intron)
     output: a rectangle proportional to actual exon'''
-    c.rectangle(start, level-5, stop-start, 10)
-    c.set_source_rgb(1, 1, 0)
+    c.rectangle(start, level-scale/4, stop-start, 2*scale/4)
+    c.set_source_rgb(0, .5, .5)
     c.fill()
 
-# set the surface dimensions
-surface_width = 100
-surface_height = 100
+def draw_region(exons, level):
+    '''draws an entire region using 'draw_exon' and 'draw_intron' fucntions
+    input: '''
+    c.move_to(0, level)
+    draw_intron(0, exons[0][0], level)
+    for i in range( 0, len(exons) ): 
+       
+       # not all exons matched to the contig, this loop skips such exons
+        if -1 in exons[i]:
+            place = c.get_current_point()
+            draw_intron(place[0], exons[i+1][0], level)
 
-normalize(exons, surface_width)
+        else:
+            try:
+                draw_exon(exons[i][0], exons[i][1] , level)
+                draw_intron(exons[i][1], exons[i+1][0], level)
+            except IndexError: # draws the final intron. If exons is final feature this intron will be length 0
+                draw_intron(exons[i][1], surface_width, level)
+
+
+
+# normalize the exons within the region dictionary
+for key in region_dictionary:
+    contig_length = region_dictionary[key][0]
+    exons= region_dictionary[key][1]
+    normalize(exons, contig_length, surface_width)
+
 
 with cairo.SVGSurface('example.svg', surface_width, surface_height) as surface:
     c = cairo.Context(surface)
-    level=surface_height/2
-    c.move_to(0, level)
+    
+    for i, key in enumerate(region_dictionary):
+        print(region_dictionary[key])
+        # set the y height for each region
+        level = (scale*i) + (surface_height/(2 * len(region_dictionary)))
 
-    # draw the first intron. If starts with exon then this intron will be length 0
-    draw_intron(0, exons[1][0], level)
+        # draw each region
+        draw_region(region_dictionary[key][1], level)
 
-    for i in range( 1, 1+len(exons) ): 
-        try:
-            draw_exon(exons[i][0], exons[i][1] , level)
-            draw_intron(exons[i][1], exons[i+1][0], level)
-        except KeyError: # draws the final intron. If exons is final feature this intron will be length 0
-            draw_intron(exons[i][1], surface_width, level)
+    # draw_region(region_dictionary['>test_contig3'][1], 50)
 
     surface.write_to_png('test.png')
