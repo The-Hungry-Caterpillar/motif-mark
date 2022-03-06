ade56a3bb7510ed35d9c05a4b20befd0dbb8b3be
Jonathan Ogata <ogata1991@gmail.com>
2022-03-05 16:22:24 -0800

    finished commenting, perhaps

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -9,8 +9,10 @@ def get_args():
     parser = argparse.ArgumentParser(description = "this script outputs a map of motifs on a regions from a fasta file, exon sequence file, and motif marker files")
     parser.add_argument('-f', '--fasta_file', help='input fasta file to be mapped')
     parser.add_argument('-m', '--motifs_file', help='input motifs file')
+    parser.add_argument('-d', '--dark', action = "store_true", help = 'dark mode')
     return parser.parse_args()
 args=get_args() 
+
 
 # grabs the contigs from input fasta file
 # header : contig
@@ -22,7 +24,8 @@ max_contig=(max([len(value) for value in fasta_dictionary.values()]))
 # get list of motifs and assign each of them random fill and border color
 with open(args.motifs_file) as f:
     motifs=f.read().splitlines()
-    motifs_list = [(motif.upper(),
+    motifs_list = [ # [ (motif 1 sequence, fill color 1, border color 1), ..., (motif N sequence, fill color N, border color N) ] 
+        (motif.upper(),
         (random.uniform(0,1), random.uniform(0,1), random.uniform(0,1)), # assign random fill color for each motif
         (random.uniform(0,1), random.uniform(0,1), random.uniform(0,1))) # assign random border color for each motif
         for motif in motifs
@@ -38,14 +41,15 @@ IUPAC = bioinfo.IUPAC
 class region:
     ''' 
     Creates class used for plotting introns and exons. 
-    Needs normalizng (doodle class function) before plotting.
+    Needs normalizng (doodle class function) before plotting.`
 
     Input: 
         - header: Contig header (used for labeling plots) 
-        - contig: Contig sequence 
+        - contig: Contig sequence where introns denoted by lowercase and exons denoted by uppercase.
     Output:
         - length: Length of contig (used for drawing last intron)
-        - exons: list of [exon start, exon end] positions. 
+        - exons: list of positions, e.g.
+            [ [exon1 start, exon1 end], [exon2 start, exon2 end], ..., [exonN start, exonN end] ] 
     '''
 
     __slots__ = ['contig', 'length', 'exons','introns', 'header']
@@ -57,6 +61,9 @@ class region:
         self.exons = self.find_exons()
 
     def find_exons(self):
+        '''
+        Looks through contig and returns start and stop positions of exons as a list
+        '''
         i=0
         exons = []
         while True:
@@ -104,6 +111,7 @@ class motif:
         - motif_positions: list of motif start and stop positions, including overlap, e.g.,
             [ [start1, stop1], [start2, stop2], ... , [startN, stopN] ] 
     '''
+
     __slots__=[ 'motif_positions','contig', 'motif', 'color','border_color']
     
     def __init__(self, motif, contig, colors, border_color):
@@ -142,14 +150,22 @@ class motif:
 
 
 class doodle:
-    '''still needs docstring'''
+    '''
+    Cairo drawing class which scales with number of items in legend (because legend is horizontal). 
+    Plots multiple contigs and features on one surface.
+
+    Input:
+        - Longest motif, used for determining legend spacing
+        - Max contig length, used for normalizing all feature lengths
+    '''
 
     # __slots__ = ['level','surface_width','surface_height', 'font_size']
 
     def __init__(self, max_motif_length, max_contig):
-        # self.surface_width=400
+        # since the legend is horizontal the width of the surface scales with number of motifs
         self.surface_width = 200 * len(motifs_list) + 200
-        self.surface_height = 200 + 200*len(fasta_dictionary)
+        self.window_size = int(self.surface_width/5)
+        self.surface_height = self.window_size + self.window_size*len(fasta_dictionary)
         self.font_size = 20
         self.label_width=max_motif_length*self.font_size
         self.max_contig=max_contig
@@ -163,6 +179,9 @@ class doodle:
         self.spacer=self.font_size/3
     
     def draw_motif(self, motif_class, height, start, stop, c):
+        '''
+        draws a motif at specified location along contig with predifined fill and border colors
+        '''
         c.rectangle(start, height-5, stop-start, 5)
         c.set_source_rgb(motif_class.color[0], motif_class.color[1], motif_class.color[2])
         c.fill_preserve()
@@ -171,37 +190,35 @@ class doodle:
         c.stroke()
 
     def stack_motifs(self, motif_class, level, c):
+        '''
+        Uses self.draw motif function to draw all instances of a motif along a contig on a given cairo surface, c.
+        This function draws the motifs at a random height around the contig as to allow for overlapping motifs.
+        '''
         height = level
         for i in range(0, len(motif_class.motif_positions)):
             start=motif_class.motif_positions[i][0]
             stop=motif_class.motif_positions[i][1]
 
-            height=random.randint(int(level-30), int(level+30))
+            height=random.randint(int(level - self.window_size/7), int(level + self.window_size/7))
             self.draw_motif(motif_class, height, start, stop, c)
                     
     def draw_intron(self, start, stop, level, c):
-        '''draws introns on cairo surface.
-        input: intron start (i.e. previous exons stop), intron stop (i.e. next exon start), y coordinate of cairo surface
-        output: a line representing intron, proportional to actual intron'''
-
+        '''
+        This function draws the introns of a contig on a given cairo surface, c.
+        '''
         c.line_to(start, level)
         c.line_to(stop, level)
-        c.set_source_rgb(0, 0, 0)
+        c.set_source_rgb(0, 0, 0) if not args.dark else c.set_source_rgb(1, 1, 1)
         c.set_line_width(1)
         c.stroke()
 
-    def draw_name(self, contig_name, level, c):
-        c.move_to(0, level-60)
-        c.set_source_rgb(0,0,0)
-        c.show_text(contig_name)
-        c.stroke()
+
 
     def draw_exon(self, start, stop, fill_colors, border_colors, level, c):
-        '''draws exons on cairo surface.
-        input: exon start, exon stop, y coordinate of cairo surface (same as draw_intron)
-        output: a rectangle proportional to actual exon'''
-
-        c.rectangle(start, level-30, stop-start, 60)
+        '''
+        This fuction draws the exons of a contif onto a given cairo surface, c.
+        '''
+        c.rectangle(start, level-int(self.window_size/4), stop-start, 2*int(self.window_size/4))
         c.set_source_rgb(fill_colors[0],fill_colors[1],fill_colors[2])
         c.fill_preserve()
         c.set_source_rgb(border_colors[0], border_colors[1], border_colors[2])
@@ -209,7 +226,9 @@ class doodle:
         c.stroke()
 
     def normalize(self, thing):
-        '''need doc string'''
+        '''
+        This function scales the locations of a given feature to the largest contig.
+        '''
 
         # transform featurelist
         for i in range(0,len(thing)):
@@ -219,12 +238,19 @@ class doodle:
                 )
 
     def draw_legend(self):
+        '''
+        This function draws a legend box. Scales with number of features being plotted.
+        '''
         c.rectangle(self.legend_start, self.legend_top, self.legend_end, self.legend_bottom)
-        c.set_source_rgb(0, 0, 0)
+        c.set_source_rgb(0, 0, 0) if not args.dark else c.set_source_rgb(1, 1, 1)
         c.set_line_width(1)
         c.stroke()
 
     def draw_label(self, feature, i, fill_colors, border_colors,c):
+        '''
+        This function draws the labels within the legend box onto a cairo surface, c.
+        Uses predefined fill and border colors.
+        '''
         c.rectangle(self.legend_start + i*self.label_width + self.spacer, #left
              self.legend_middle-7, #top
              self.font_size, #right
@@ -236,23 +262,28 @@ class doodle:
         x=c.get_current_point()[0]
         c.stroke()        
         c.move_to(x + self.spacer + self.font_size, self.legend_middle + .5*self.font_size)
-        c.set_source_rgb(0,0,0)
+        c.set_source_rgb(0,0,0) if not args.dark else c.set_source_rgb(1, 1, 1)
         c.show_text(feature)
+        c.stroke()
+
+    def draw_name(self, contig_name, level, c):
+        '''This function draws the name of the contig above contig on a given cairo surface, c.'''
+        c.move_to(0, int(level-self.window_size/3))
+        c.set_source_rgb(0,0,0) if not args.dark else c.set_source_rgb(1, 1, 1)
+        c.show_text(contig_name)
         c.stroke()
 
 draw = doodle(max_motif_length,max_contig)
 with cairo.SVGSurface('example.svg', draw.surface_width, draw.surface_height) as surface:
     c = cairo.Context(surface)
-    
-    # set font parameters
-    c.set_source_rgb(0, 0, 0)
     c.set_font_size(draw.font_size)
-    c.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
-    
+
     # draw legend stuff
-    draw.draw_legend() # legend box
+    # legend box
+    draw.draw_legend() 
     label_number=0
-    draw.draw_label("Exon", label_number, (0.5,0.5,0.5), (0,0,0), c) # legend exon
+    # legend exon
+    draw.draw_label("Exon", label_number, (0.5,0.5,0.5), (0,0,0), c) if not args.dark else draw.draw_label("Exon", label_number, (0.8,0.8,0.8), (1,1,1), c) 
     label_number+=1
     
     # put motifs on legend
@@ -263,22 +294,32 @@ with cairo.SVGSurface('example.svg', draw.surface_width, draw.surface_height) as
 
     for h,key in enumerate(fasta_dictionary):
         
-        level = h*200 + 250
+        # the h factor separates contigs vertically in cairo space
+        level = h*draw.window_size + draw.window_size
+
+        # initialize a region object based on the current contig
         gene = region(key, fasta_dictionary[key])
+
+        # normalize all the exons and total length of current contig
         draw.normalize(gene.exons)
         length=round(draw.surface_width * gene.length/draw.max_contig, 0)
 
+        # draw name of contig above contig
         draw.draw_name(key,level, c)
+
+        # draw first intron 
         draw.draw_intron(0, gene.exons[0][0], level, c)
 
+        # draws all introns and exons of a contig
         for i in range( 0, len(gene.exons) ): 
             try:
-                draw.draw_exon(gene.exons[i][0], gene.exons[i][1], (0.5, 0.5, 0.5), (0, 0, 0), level, c)
+                draw.draw_exon(gene.exons[i][0], gene.exons[i][1], (0.5, 0.5, 0.5), (0, 0, 0), level, c) if not args.dark else draw.draw_exon(gene.exons[i][0], gene.exons[i][1], (0.8, 0.8, 0.8), (1, 1, 1), level, c)
                 draw.draw_intron(gene.exons[i][1], gene.exons[i+1][0], level, c)
             
             except IndexError: # draws the final intron. If exons is final feature this intron will be length 0
                 draw.draw_intron(gene.exons[i][1], length, level, c)
 
+        # draws all motifs associated with current contig
         for motiv in motifs_list:
             moti = motif(motiv[0], gene.contig, motiv[1], motiv[2])
             draw.normalize(moti.motif_positions)
